c9a1de306163e84dbfde4dafa6fc275c8927df6b
skibidi-toilet-crypto <rcborstudent@gmail.com>
2026-06-04 13:42:20 +0300

    a

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const messagesStorage = [{ login: 'GGG', content: 'Hello' }];
+//const messagesStorage = [{ login: 'GGG', content: 'Hello' }];
 
 const pathToIndex = path.join(__dirname, 'static', 'index.html');
 const pathToCss = path.join(__dirname, 'static', 'style.css');
@@ -30,19 +30,55 @@ const { Server } = require('socket.io');
 const io = new Server(server);
 
 io.on('connection', async (socket) => {
-    const guestNickname = 'Guest' + Math.floor(Math.random() * 1000);
-    console.log(`${guestNickname} connected. id - ${socket.id}`);
+    const currentNickname = 'Guest' + Math.floor(Math.random() * 1000);
+    const guestPassword = 'password' + Math.floor(Math.random() * 1000);
 
-    socket.emit('all_messagess', messagesStorage);
+    let currentUser = null;
 
+    console.log('A user connected: ' + currentNickname);
+
+    try {
+        await db.addUser({
+            login: currentNickname,
+            password: guestPassword
+        })
+        console.log(`BD user ${currentNickname} made`);
+
+        const token = await db.getAuthToken({
+            login: currentNickname,
+            password: guestPassword
+        });
+        if (token) {
+            currentUserId = parseInt(token.split('-')[0]);
+            console.log(`BD ID from base: ${currentUserId}`);
+        }
+
+    } catch (error) {
+        console.error('Error BD creation or token retrieval:', error);
+        currentUserId = null;
+    }
+    try {
+        const messages = await db.getMessages();
+        socket.emit('all_messages', messages);
+    } catch (error) {
+        console.error('Error fetching messages:', error);
+    }
     socket.on('new_message', async (message) => {
-        const newMessageObj = { login: guestNickname, content: message };
-        messagesStorage.push(newMessageObj);
+        if (!currentUserId) {
+            console.error('User not authenticated, cannot send message');
+            return;
+        }
+        try {
+            await db.addMessage({ message, currentUserId });
+            console.log(`BD sawed masseges ID ${currentUserId} (${currentNickname})`)
 
-        io.emit('message', newMessageObj.login + ':' + newMessageObj.content);
-    });
+            io.emit('message', currentNickName + ': ' + message);
+        } catch (e) {
+            console.error('Error saving message:', e);
+        }
+    })
+})
 
-});
 server.listen(3000, () => {
     console.log('Server running on port 3000');
 });  
